1fdc416e37f586ff92bbce1c8ad734fdc13128c2
mohan g s <mohangs89@gmail.com>
2024-02-27 14:15:38 +0530

    Integrating appwrite and listing items

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {Client, Databases} from 'appwrite';
+import { useEffect, useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const BAAS_ENDPOINT = '';
+const PROJECT_ID = '';
+const DATABASE_ID = '';
+const COLLECTOIN_ID = '';
+
+const client = new Client();
+  client
+    .setEndpoint(BAAS_ENDPOINT)
+    .setProject(PROJECT_ID);
+
+const db = new Databases(client);
+
+function Card(params){
+  const {$id, title, url, context, category, tag} = params;
+
+  const navigate = (url) => {
+    window.open(url, "_blank");
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="column is-one-third" onClick={() => navigate(url)}>
+        <div className="card" key={$id}>
+          <div className="card-content">
+            <div className="content">
+              {title}
+            </div>
+          </div>
+          <footer className="card-footer">
+            <a href="#" className="card-footer-item">{context}</a>
+            <a href="#" className="card-footer-item">{category}</a>
+            <a href="#" className="card-footer-item">{tag}</a>
+          </footer>
+        </div>
+    </div>
+  )
+}
+
+function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    async function getBookmarks(){
+      const response = await db.listDocuments(DATABASE_ID, COLLECTOIN_ID);
+      setData(response);
+    }
+
+    getBookmarks();
+    
+  }, [])
+
+  if(!data){
+    return null;
+  }
+
+  return (
+    <div className="columns">
+      {data.documents.map((datum) => <Card key={datum.$id} {...datum} />)}
+    </div>
   )
 }
 
